764962424b972649d6b7aef20138eca5bf7893c7
Even Solberg <even.solberg@gmail.com>
2024-03-17 09:00:02 -0700

    refactor: Lint fixes

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
+use clap::{value_parser, Arg, ArgAction, ArgMatches, Command};
+use futures::{stream, StreamExt};
 use std::{
     net::{IpAddr, SocketAddr},
     time::Duration,
 };
-use clap::{Command, Arg, ArgAction, ArgMatches, value_parser};
-use futures::{stream, StreamExt};
 use tokio::net::TcpStream;
 
 /// All of the values required for a single pool of scanners
@@ -25,7 +25,7 @@ async fn main() -> Result<(), anyhow::Error> {
     // Convert CLI args into `ScanConfig`
     let cli = init_cli();
     let args = cli.get_matches();
-    let scan_config = init_scan_config(args);
+    let scan_config = init_scan_config(&args);
 
     // Begin scan
     scan(&scan_config).await;
@@ -35,7 +35,7 @@ async fn main() -> Result<(), anyhow::Error> {
 
 /// Creates and configures the CLI argument structure
 fn init_cli() -> Command {
-    let cli = Command::new("eyes")
+    Command::new("eyes")
         // <target>
         .arg(
             Arg::new("target")
@@ -78,45 +78,44 @@ fn init_cli() -> Command {
                 .default_value("3"),
         )
         // print help if no cli args are given
-        .arg_required_else_help(true);
-
-    return cli
+        .arg_required_else_help(true)
 }
 
 /// Converts CLI args into a `ScanConfig` instance
-fn init_scan_config(args: ArgMatches) -> ScanConfig {
+fn init_scan_config(args: &ArgMatches) -> ScanConfig {
     // Prepare host address
     let target = args.get_one::<String>("target").expect("required");
-    let sockaddr: SocketAddr = format!("{}:0", target).parse().unwrap();
+    let sockaddr: SocketAddr = format!("{target}:0").parse().unwrap_or(SocketAddr::new(
+        IpAddr::V4(std::net::Ipv4Addr::new(127, 0, 0, 1)),
+        80,
+    ));
     let target_ip = sockaddr.ip();
 
     // Optional CLI params
     let verbose = args.get_flag("verbose");
-    let concurrency = match args.get_one::<usize>("concurrency") {
-        Some(c) => usize::from(*c),
-        _ => 1000
-    };
-    let ports = match args.get_one::<String>("ports") {
-        Some(p) => parse_ports(String::from(p)),
-        _ => parse_ports(String::from("1-1024"))
-    };
-    let timeout = match args.get_one::<u64>("timeout") {
-        Some(t) => u64::from(*t),
-        _ => 3
-    };
+    let concurrency = args.get_one::<usize>("concurrency").map_or(1000, |c| *c);
+    let ports = args.get_one::<String>("ports").map_or_else(
+        || parse_ports(&String::from("1-1024")),
+        |p| parse_ports(p.as_str()),
+    );
+    let timeout = args.get_one::<u64>("timeout").map_or(3, |t| *t);
 
     // Build config
     let sc = ScanConfig {
         target_ip,
         ports,
-        verbose,
         concurrency,
         timeout,
+        verbose,
     };
 
     // Log the config params
     if sc.verbose {
-        println!("[eyes] Scanning {} ports on {}", sc.ports.len(), sc.target_ip);
+        println!(
+            "[eyes] Scanning {} ports on {}",
+            sc.ports.len(),
+            sc.target_ip
+        );
         println!("[eyes] Concurrency: {}", sc.concurrency);
         println!("[eyes] Timeout: {}", sc.timeout);
     }
@@ -125,34 +124,38 @@ fn init_scan_config(args: ArgMatches) -> ScanConfig {
 }
 
 /// Converts user-provided description of ports into `Vec` of unique numbers
-/// 
+///
 ///  Ports can be expressed as:
 ///    * list of numbers: `eyes <target> -p 22,80,1336`
 ///    * range of numbers: `eyes <target> -p 22-80`
 ///    * mix of both: `eyes <target> -p 22,80,8000-8099,443,8443,3000-3443`
-fn parse_ports(ports_arg: String) -> Vec<u16> {
+fn parse_ports(ports_arg: &str) -> Vec<u16> {
     let mut ports: Vec<u16> = Vec::new();
 
     // List of ports found: `x,y,z`
-    if ports_arg.contains(",") {
-        let ps = ports_arg.split(",");
+    if ports_arg.contains(',') {
+        let ps = ports_arg.split(',');
         for p in ps {
             // List item is port range: `x-z`
-            if p.contains("-") {
-                let range: Vec<u16> = p.split('-').map(|x: &str| x.parse::<u16>().unwrap()).collect();
+            if p.contains('-') {
+                let range: Vec<u16> = p
+                    .split('-')
+                    .map(|x: &str| x.parse::<u16>().unwrap_or_default())
+                    .collect();
                 ports.extend(range[0]..=range[1]);
             }
             // List item is single port: `x`
-            else {
-                if let Ok(p) = p.parse::<u16>() {
-                    ports.push(p);
-                }
+            else if let Ok(p) = p.parse::<u16>() {
+                ports.push(p);
             }
         }
     }
     // Range of ports found: `x-z`
-    else if ports_arg.contains("-") {
-        let range: Vec<u16> = ports_arg.split('-').map(|x: &str| x.parse::<u16>().unwrap()).collect();
+    else if ports_arg.contains('-') {
+        let range: Vec<u16> = ports_arg
+            .split('-')
+            .map(|x: &str| x.parse::<u16>().unwrap_or_default())
+            .collect();
         ports.extend(range[0]..=range[1]);
     }
     // Single port found: `x`
@@ -162,7 +165,7 @@ fn parse_ports(ports_arg: String) -> Vec<u16> {
     // Default to range of ports: `1-1024`
     else {
         ports.extend(1..=1024);
-    }    
+    }
 
     ports
 }
@@ -182,10 +185,14 @@ async fn scan(sc: &ScanConfig) {
 /// Attempts to open a connection to target on the provided `port`
 async fn open_port(c: &ScanConfig, port: u16) {
     let timeout = Duration::from_secs(c.timeout);
-    let socket_address = SocketAddr::new(c.target_ip.clone(), port);
+    let socket_address = SocketAddr::new(c.target_ip, port);
 
     match tokio::time::timeout(timeout, TcpStream::connect(&socket_address)).await {
-        Ok(Ok(_)) => println!("{}: open", port),
-        _ => if c.verbose { println!("{}: closed", port); }  
+        Ok(Ok(_)) => println!("{port}: open"),
+        _ => {
+            if c.verbose {
+                println!("{port}: closed");
+            }
+        }
     }
 }
